dd4e314c21d1b3133245cd89b1480309f04a34bc
sqoor <alsqoorAbdullah@gmail.com>
2019-08-09 23:05:09 +0300

    created the key components (Search, Detail, Item, List)

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class VideoDetail extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default VideoDetail
+
+
