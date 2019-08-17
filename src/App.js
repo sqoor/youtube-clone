f46a25c7e147c16b43f1d26ea81c55ca5f8d6c50
sqoor <alsqoorAbdullah@gmail.com>
2019-08-17 16:33:33 +0300

    reafactored App component convert to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import SearchBar from './components/SearchBar';
@@ -8,27 +8,15 @@ import Results from './components/Results';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <SearchBar searchVideos={this.searchVideos} />
-        <BrowserRouter>
-          <Route exact path="/" component={Home} />
-          <Route 
-            exact
-            path="/watch" 
-            component={() => <Watch />} 
-          />
-           <Route 
-            exact
-            path="/results" 
-            component={() => <Results />} 
-          />
-        </BrowserRouter>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/watch" component={() => <Watch />} />
+        <Route exact path="/results" component={() => <Results />} />
+      </BrowserRouter>
+    </div>
+  );
 }
-
-export default App;
